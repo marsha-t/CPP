573fb9f2f81be78ed59d33624dfd8a35aac6c6db
Marsha <151203981+marsha-t@users.noreply.github.com>
2024-09-21 05:39:35 +0000

    add cpp00 ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+/**
+ * @brief Converts arguments into uppercase
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str = argv[i];
+			for (std::string::size_type j = 0; j < str.length(); j++)
+			{
+				std::cout << std::toupper(static_cast<unsigned char>(str[j]));
+			}
+			if (i < argc - 1)
+				std::cout << ' '; 
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
