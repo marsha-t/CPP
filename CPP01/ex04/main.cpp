9c15be00d4516d52b73014999431d58353b253ce
Marsha Teo <mateo@lab1r1s12.42abudhabi.ae>
2024-10-14 14:00:59 +0400

    fix error msg when empty file

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-////////////////////////////////////////////////////////
-/// ADD validation for permissions in file; empty line in file; 
-// tabs and spaces in file 
-////////////////////////////////////////////////////////
+
 int	main(int argc, char **argv)
 {
 	if (argc != 4)
@@ -14,19 +11,19 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 	
-	std::string input[3]; 
+	std::string input[4]; 
 	for (int i = 0; i < 3; ++i)
 	{
 		input[i] = argv[i + 1];
 	}
-	input[0] += ".replace";
+	input[3] = input[0] + ".replace";
 	std::ifstream	input_file(argv[1]);
 	if (!input_file.is_open())
 	{
 		std::cerr << "Error opening " << argv[1] << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::ofstream	output_file(input[0].c_str(), std::ios::out | std::ios::trunc);
+	std::ofstream	output_file(input[3].c_str(), std::ios::out | std::ios::trunc);
 	if (!output_file.is_open())
 	{
 		input_file.close();
@@ -34,7 +31,7 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 	std::string	line;
-	if (std::getline(input_file, line, '\0')) // don't use while becuase s1 could contain new line also 
+	if (std::getline(input_file, line, '\0'))
 	{
 		size_t	position = line.find(input[1]);
 		while (position != std::string::npos)
@@ -47,11 +44,10 @@ int	main(int argc, char **argv)
 	}
 	else
 	{
-		std::cerr << input[0] << "is empty" << std::endl;
+		std::cerr << input[0] << " is empty" << std::endl;
 	}
 	input_file.close();
 	output_file.close();
-
 	return (EXIT_SUCCESS);
 }
 
