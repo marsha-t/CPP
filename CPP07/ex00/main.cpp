85936d2c85e3a2319ee70060246dc329b5469887
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-03-13 06:35:55 +0000

    add CPP07/ex00

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,78 @@
+#include "whatever.hpp"
+
+testClass::testClass(void): _value(0) {}
+
+testClass::testClass(int value): _value(value) {}
+
+testClass::testClass(const testClass &obj): _value(obj.getValue()) {}
+
+testClass::~testClass(void) {}
+
+testClass	&testClass::operator=(const testClass &obj)
+{
+	if (this != &obj)
+	{
+		_value = obj.getValue();
+	}
+	return (*this);
+}
+
+bool	testClass::operator>(const testClass &obj) const
+{
+	return (_value > obj.getValue());
+}
+
+bool	testClass::operator<(const testClass &obj) const
+{
+	return (_value < obj.getValue());
+}
+
+bool	testClass::operator<=(const testClass &obj) const
+{
+	return (!(*this > obj));
+}
+
+bool	testClass::operator>=(const testClass &obj) const
+{
+	return (!(*this < obj));
+}
+
+int	testClass::getValue(void) const
+{
+	return (_value);
+}
+
+void	subjectTests(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}
+
+void	classTest(void)
+{
+	testClass	a = testClass(42);
+	testClass	b = testClass(21);
+	std::cout << "Before: a = " << a.getValue() << ", b = " << b.getValue() << std::endl;
+	::swap(a, b);
+	std::cout << "After: a = " << a.getValue() << ", b = " << b.getValue() << std::endl;
+	std::cout << "min (a, b) = " << ::min(a, b).getValue() << std::endl;
+	std::cout << "max (a, b) = " << ::max(a, b).getValue() << std::endl;
+}
+
+int	main( void ) {
+	
+	subjectTests();
+	classTest();
+	return (EXIT_SUCCESS);
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,52 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+void	swap(T &x, T &y)
+{
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+T const &min(T const x, T const y)
+{
+	return ((x <= y ? x : y));
+}
+
+template <typename T>
+T const &max(T const x, T const y)
+{
+	return ((x >= y ? x : y));
+}
+
+class	testClass
+{
+	public:
+		// Constructors
+		testClass(void);
+		testClass(int value);
+		testClass(const testClass &obj);
+
+		// Destructor
+		~testClass(void);
+
+		// Operators
+		testClass	&operator=(const testClass &obj);
+		bool	operator>(const testClass &obj) const;
+		bool	operator<(const testClass &obj) const;
+		bool	operator>=(const testClass &obj) const;
+		bool	operator<=(const testClass &obj) const;
+
+		// Getters
+		int	getValue(void) const;
+
+	private:
+		int _value;
+};
+
+#endif
