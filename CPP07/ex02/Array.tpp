ceda2dc10a3c5ac5dac20098075a4c19d655b183
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-03-18 18:04:36 +0000

    finish ex02 and tests

--- CPP07/ex02/Array.tpp
+++ CPP07/ex02/Array.tpp
@@ -1,28 +1,60 @@
 #ifndef	ARRAY_TPP
 #define ARRAY_TPP
 
+#include <stdexcept>
+
 template <typename T>
 class	Array
 {
+	private:
+		unsigned int	_size;
+		T				*_arr;
 	public:
-		// Constructors
-		Array<T>(void): _n(0), _arr(0) {}
-		Array<T>(unsigned int n): _n(n), _arr(new T[_n]) {}
-		Array<T>(const Array<T> &obj);
-		
+		// Constructor
+		Array<T>(void): _size(0), _arr(0) {}
+		Array<T>(unsigned int n): _size(n), _arr(new T[n]()) {}
+		Array<T>(const Array<T> &obj): _size(obj._size)
+		{
+			_arr = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+			{
+				_arr[i] = obj._arr[i];
+			}
+		}
+
 		// Destructor
-		~Array<T>(void) {}
+		~Array<T>(void) { delete[] _arr; }
 
 		// Operator
-		Array<T>	&operator=(const Array<T> &obj);
-		T	&operator[](unsigned int index);
-
+		Array<T>	&operator=(const Array<T> &obj)
+		{
+			if (this != &obj)
+			{
+				delete[] _arr;
+				_size = obj._size;
+				_arr = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+				{
+					_arr[i] = obj._arr[i];
+				}
+			}
+			return (*this);
+		}
+		T	&operator[](unsigned int index)
+		{
+			if (index < 0 || index >= _size)
+				throw std::out_of_range ("Index is out of bounds");
+			return (_arr[index]);
+		}
+		const T	&operator[](unsigned int index) const
+		{
+			if (index < 0 || index >= _size)
+				throw std::out_of_range ("Index is out of bounds");
+			return (_arr[index]);
+		}
 		// Other functions
-		unsigned int	size(void) const { return _n; };
-
-	private:
-		unsigned int	_n;
-		T				*_arr;
+		unsigned int	size(void) const { return (_size); }
+	
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,5 +1,4 @@
 #include "Array.tpp"
-#include "Array.hpp"
 
 #include <iostream>
 #include <string>
@@ -8,33 +7,180 @@
 
 void	testIntArray(void)
 {
-	// int	*a = new int();
-	// std::cout << *a << std::endl;
+	std::cout << "--- EMPTY ARRAY ---" << std::endl;
 	Array<int>	emptyArray;
 	std::cout << "Size of empty array: " << emptyArray.size() <<std::endl;
+	try
+	{
+		std::cout << emptyArray[0] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 
+
+	std::cout << "--- Int Array (5) ---" << std::endl;
 	Array<int>	intArray(5);
-	std::cout << intArray.size() <<std::endl;
+	std::cout << "Size of int[5]: " << intArray.size() <<std::endl;
+	for (unsigned int i = 0; i < intArray.size(); i++)
+		std::cout << intArray[i] << " " << std::flush;
+	
 	intArray[2] = 2;
-	std::cout << intArray[2] << std::endl;
+	std::cout << std::endl; 
+	for (unsigned int i = 0; i < intArray.size(); i++)
+		std::cout << intArray[i] << " " << std::flush;
+	std::cout << std::endl; 
+
 	try
 	{
 		intArray[5] = 2;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
+
+		
+	std::cout << "--- Copy ---" << std::endl;
+	Array<int> copyIntArray(intArray);
+	std::cout << "Size of copy: " << copyIntArray.size() <<std::endl;
+	for (unsigned int i = 0; i < copyIntArray.size(); i++)
+	{
+		std::cout << copyIntArray[i] << " " << std::flush;
+	}
+	std::cout << std::endl;
+	
+	Array<int>	newIntArray(3);
+	std::cout << "Size of newIntArray: " << newIntArray.size() << std::endl;
+	for (unsigned int i = 0; i < newIntArray.size(); i++)
+	{
+		std::cout << newIntArray[i] << " " <<std::flush;
+	}
+	std::cout << std::endl;
+
+	newIntArray = intArray; 
+	std::cout << "Size of newIntArray: " << newIntArray.size() << std::endl;
+	for (unsigned int i = 0; i < newIntArray.size(); i++)
+	{
+		std::cout << newIntArray[i] << " " <<std::flush;
+	}
+	std::cout << std::endl;
 }
 
 void	testStringArray(void)
 {
+	std::cout << "--- EMPTY ARRAY ---" << std::endl;
+	Array<std::string>	emptyArray;
+	std::cout << "Size of empty array: " << emptyArray.size() <<std::endl;
+	try
+	{
+		std::cout << emptyArray[0] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
 
+
+	std::cout << "--- String Array (5) ---" << std::endl;
+	Array<std::string>	strArray(5);
+	std::cout << "Size of str[5]: " << strArray.size() <<std::endl;
+	for (unsigned int i = 0; i < strArray.size(); i++)
+	{
+		if (strArray[i] == "")
+			std::cout << "_ " << std::flush;
+	}
+	
+	strArray[2] = "a";
+	std::cout << std::endl; 
+	for (unsigned int i = 0; i < strArray.size(); i++)
+	{
+		if (strArray[i] == "")
+			std::cout << "_ " << std::flush;
+		else
+			std::cout << strArray[i] << " " << std::flush;
+	}
+	std::cout << std::endl; 
+
+	try
+	{
+		strArray[5] = 2;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+
+		
+	std::cout << "--- Copy ---" << std::endl;
+	Array<std::string> copyStrArray(strArray);
+	std::cout << "Size of copy: " << copyStrArray.size() <<std::endl;
+	for (unsigned int i = 0; i < copyStrArray.size(); i++)
+	{
+		if (strArray[i] == "")
+			std::cout << "_ " << std::flush;
+		else
+			std::cout << copyStrArray[i] << " " << std::flush;
+	}
+	std::cout << std::endl;
+	
+	Array<std::string>	newStrArray(3);
+	std::cout << "Size of newStrArray: " << newStrArray.size() << std::endl;
+	for (unsigned int i = 0; i < newStrArray.size(); i++)
+	{
+		if (newStrArray[i] == "")
+			std::cout << "_ " << std::flush;
+		else
+			std::cout << newStrArray[i] << " " <<std::flush;
+	}
+	std::cout << std::endl;
+
+	newStrArray = strArray; 
+	std::cout << "Size of newStrArray: " << newStrArray.size() << std::endl;
+	for (unsigned int i = 0; i < newStrArray.size(); i++)
+	{
+		if (strArray[i] == "")
+			std::cout << "_ " << std::flush;
+		else
+			std::cout << newStrArray[i] << " " <<std::flush;
+	}
+	std::cout << std::endl;
 }
 
-int	main(void)
+void	testConstArray(void)
 {
-	testIntArray();
-	testStringArray();	
+	const Array<int> intArray(5);
+	std::cout << "Size of int[5]: " << intArray.size() <<std::endl;
+	for (unsigned int i = 0; i < intArray.size(); i++)
+		std::cout << intArray[i] << " " << std::flush;
+	std::cout << std::endl;
+	// intArray[2] = 2;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		int	test = atoi(argv[1]);
+		switch (test)
+		{
+			case 0:
+			{
+				testIntArray();
+				break ; 
+			}
+			case 1:
+			{
+				testStringArray();
+				break ;
+			}
+			case 2:
+			{
+				testConstArray();
+				break ;
+			}
+		}
+	}
 	return (EXIT_SUCCESS);
 }
