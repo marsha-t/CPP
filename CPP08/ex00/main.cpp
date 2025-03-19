d2fba4899c1b8c9588c289ff131c16979e63c3e3
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-03-19 16:15:05 +0000

    update test checks and return status

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -54,26 +54,24 @@ int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		int	test = atoi(argv[1]);
+		int	test = std::atoi(argv[1]);
+		if (test < 0 || test > 2)
+			return (1);
 		int	arr[] = {1, 2, 3, 4};
 		switch (test)
 		{
 			case 0:
-			{
 				testVector(arr);
 				break ;
-			}
 			case 1:
-			{
 				testList(arr);
 				break ;
-			}
 			case 2:
-			{
 				testDeque(arr);
 				break ;
-			}
 		}
+		return (0);
 	}
-	return (0);
+	return (1);
 }
+
